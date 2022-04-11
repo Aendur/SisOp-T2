6077a88114ea2b3ba6ae8c33ef5ae986760c3e71
Aendur <Aendur@users.noreply.github.com>
2022-04-11 17:29:20 -0300

    SharedMemory class

---
+++ header/shared_memory.h
@@ -0,0 +1,34 @@
+#ifndef SHARED_MEMORY_H
+#define SHARED_MEMORY_H
+
+#include <cstddef>
+
+class SharedMemory {
+private:
+	int    _key = -1;        // Create / Retrieve / Remove
+	int    _id = -1;         // Create / Retrieve / Remove
+	size_t _size = 0;        // Fetch
+	void*  _addr = nullptr; // Attach / Detach
+
+	void Fetch(void);
+	void Attach(void);
+	void Detach(void);
+public:
+	// Requests a new shared memory block with given size
+	void Create(int key, size_t size);
+
+	// Requests an existing shared memory block
+	void Retrieve(int key);
+
+	// Requests an existing shared memory block to be removed
+	void Dispose(void);
+
+	inline int    id(void) const { return _id; }
+	inline int    key(void) const { return _key; }
+	inline size_t size(void) const { return _size; }
+	inline void*  addr(void) const { return _addr; }
+};
+
+
+#endif
+
